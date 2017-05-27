9dbb52b44621be5835e7f71f5e8604c36662114d
Lucca <julienlucca@gmail.com>
2017-05-27 01:41:03 -0300

    Correct coherence behavior
    closes #1

--- lib/ashes/web/coherence_web.ex
+++ lib/ashes/web/coherence_web.ex
@@ -3,9 +3,9 @@ defmodule Ashes.Coherence.Web do
 
   def view do
     quote do
-      
       use Phoenix.View, root: "lib/ashes/web/templates/coherence"
       # Import convenience functions from controllers
+
       import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
 
       # Use all HTML functionality (forms, tags, etc)
@@ -15,6 +15,26 @@ defmodule Ashes.Coherence.Web do
       import Ashes.Web.ErrorHelpers
       import Ashes.Web.Gettext
       import Ashes.Coherence.ViewHelpers
+    end
+  end
+
+  def controller do
+    quote do
+      use Phoenix.Controller, except: [layout_view: 2] #, namespace: BlogPhx.Web
+      use Coherence.Config
+      use Timex
+
+      import Ecto
+      import Ecto.Query
+      import Plug.Conn
+      import Ashes.Web.Router.Helpers
+      import Ashes.Web.Gettext
+      import Coherence.ControllerHelpers
+
+      alias Coherence.Config
+      alias Coherence.ControllerHelpers, as: Helpers
+
+      require Redirects
     end
   end
 
