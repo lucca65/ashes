2a776733c7f2735ed1a42704ac57e04f4bae1eff
Lucca <julienlucca@gmail.com>
2017-05-28 16:02:45 -0300

    Remove node_modules

--- assets/node_modules/phoenix/priv/templates/phx.gen.context/context.ex
+++
@@ -1,8 +0,0 @@
-defmodule <%= inspect context.module %> do
-  @moduledoc """
-  The boundary for the <%= context.name %> system.
-  """
-
-  import Ecto.{Query, Changeset}, warn: false
-  alias <%= inspect schema.repo %>
-end
--- assets/node_modules/phoenix/priv/templates/phx.gen.html/view.ex
+++
@@ -1,3 +0,0 @@
-defmodule <%= inspect context.web_module %>.<%= inspect schema.alias %>View do
-  use <%= inspect context.web_module %>, :view
-end
